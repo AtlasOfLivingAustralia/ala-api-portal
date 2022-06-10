7a7f2fd60a81da0a5366d25d5297fc273b542b81
Bruce Hyslop <bruce.hyslop@csiro.au>
2022-06-10 13:24:57 +1000

    removed config for un-deployed api specs

--- slate/source/openapi/swagger-initializer.js
+++ slate/source/openapi/swagger-initializer.js
@@ -4,11 +4,11 @@ window.onload = function() {
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
       urls: [
-        { name: "alerts", url: "./specs/alerts-rest.yaml"},
-        { name: "biocache", url: "./specs/biocache-http.yaml"},
-        { name: "ecodata", url: "./specs/ecodata-http.yaml"},
-        { name: "images", url: "./specs/images-rest.yaml"},
-        { name: "userdetails", url: "./specs/userdetails-rest.yaml"}
+        // { name: "alerts", url: "./specs/alerts-rest.yaml"},
+        // { name: "biocache", url: "./specs/biocache-http.yaml"},
+        // { name: "ecodata", url: "./specs/ecodata-http.yaml"},
+        // { name: "images", url: "./specs/images-rest.yaml"},
+        { name: "userdetails", url: "./specs/userdetails-oas30.json"}
       ],
       dom_id: '#swagger-ui',
       deepLinking: true,

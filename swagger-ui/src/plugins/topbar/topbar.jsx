35aa2fdcaa406c91ad2e1fb3c6dd618ce137002b
Bruce Hyslop <bruce.hyslop@csiro.au>
2022-06-10 11:25:46 +1000

    added back icon for swagger created slate logo link to ALA home

--- swagger-ui/src/plugins/topbar/topbar.jsx
+++ swagger-ui/src/plugins/topbar/topbar.jsx
@@ -153,7 +153,7 @@ export default class Topbar extends React.Component {
         <div className="wrapper">
           <div className="topbar-wrapper">
             <Link href="../">
-              <img height="40" src={ Logo } alt="Swagger UI"/>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" viewBox="0 0 320 512"><path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z" stroke="white" fill="white" /></svg> <img height="40" src={ Logo } alt="Swagger UI"/>
             </Link>
             <form className="download-url-wrapper" onSubmit={formOnSubmit}>
               {control.map((el, i) => cloneElement(el, { key: i }))}

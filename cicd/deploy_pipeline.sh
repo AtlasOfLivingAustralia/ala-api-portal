25dbac6c25b3b2ed775645cd509b3cfde92b513d
Joe Lipson <joe.lipson@csiro.au>
2022-07-22 10:31:41 +1000

    fix bucket name
    add commit id tag to pipeline

--- cicd/deploy_pipeline.sh
+++ cicd/deploy_pipeline.sh
@@ -4,24 +4,29 @@ set -ueo pipefail
 ###
 # deploy the codepipeline for API documentation
 
-# get the environment
+# get the branch
 branch=$(git branch --show-current)
 echo branch: $branch
 
+# get the commit_id
+commit_id=$(git rev-parse HEAD)
+echo commit_id: $commit_id
+
+# get the environment based on the branch
 environment=$(./branch_2_env.py --branch $branch)
 echo environment: $environment
 
-# generate environment vars
+# load environment vars
 ./gen_env_vars.py --env $environment > env.txt
 source env.txt
 rm env.txt
 
 # deploy/update the template
-echo "Deploying the output template"
+echo "Deploying the pipeline template"
 aws cloudformation deploy \
     --template-file pipeline.yaml \
     --stack-name $PIPELINE_STACK_NAME \
-    --tags $PIPELINE_AWS_TAGS environment=$environment branch=$branch \
+    --tags $PIPELINE_AWS_TAGS environment=$environment branch=$branch commit_id=$commit_id \
     --region $REGION \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameter-overrides \

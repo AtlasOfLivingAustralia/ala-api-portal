7db2131ae3a27ae39ba366146f68c1dcfd1e46c6
yasima-csiro <yasima.kankanamge@csiro.au>
2022-08-02 11:29:56 +1000

    Fix API spec names

--- slate/source/openapi/swagger-initializer.js
+++ slate/source/openapi/swagger-initializer.js
@@ -4,12 +4,12 @@ window.onload = function() {
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
       urls: [
-        { name: "alerts", url: "./specs/alerts.json"},
+        // { name: "alerts", url: "./specs/alerts.json"},
         // { name: "biocache", url: "./specs/biocache-http.yaml"},
         // { name: "ecodata", url: "./specs/ecodata-http.yaml"},
         // { name: "images", url: "./specs/images-rest.yaml"},
-        { name: "userdetails", url: "./specs/userdetails-oas30.json"},
-        { name: "logger", url: "./specs/logger.json"}
+        { name: "userdetails", url: "./specs/userdetails-rest.json"},
+        // { name: "logger", url: "./specs/logger.json"}
       ],
       dom_id: '#swagger-ui',
       deepLinking: true,

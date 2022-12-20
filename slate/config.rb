3d49b11f534264377392df7f3262cca5db105e48
Joe Lipson <joe.lipson@csiro.au>
2022-12-20 13:42:31 +1100

    limiting the lang caused an error

--- slate/config.rb
+++ slate/config.rb
@@ -41,7 +41,7 @@ set :relative_links, true
 
 set :build_dir, '../build'
 
-activate :i18n, :mount_at_root => :ENV['ENVIRONMENT'], :langs => [:ENV['ENVIRONMENT']]
+activate :i18n, :mount_at_root => :ENV['ENVIRONMENT']
 
 # Build Configuration
 configure :build do

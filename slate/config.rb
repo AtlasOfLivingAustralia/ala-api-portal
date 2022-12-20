ce3c56b28255f6bba0d242d51a876564f3adba60
Joe Lipson <joe.lipson@csiro.au>
2022-12-20 13:29:11 +1100

    try limiting the language files

--- slate/config.rb
+++ slate/config.rb
@@ -41,7 +41,7 @@ set :relative_links, true
 
 set :build_dir, '../build'
 
-activate :i18n, :mount_at_root => :ENV['ENVIRONMENT'], :path => ":locales"
+activate :i18n, :mount_at_root => :ENV['ENVIRONMENT'], :langs => [:ENV['ENVIRONMENT']]
 
 # Build Configuration
 configure :build do
